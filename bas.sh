b45bafea5f4577fc2dbe17aec372501ed1a927e1
Marcin Witalis <marcin.witalis@sap.com>
2019-07-15 20:26:04 +0200

    fix minikube support

--- bas.sh
+++ bas.sh
@@ -3,6 +3,8 @@
 ITERATION_NUMBER=${ITERATION_NUMBER:-5}
 ASSET_NUMBER=${ASSET_NUMBER:-10}
 
+sed -i 's/__MINIKUBE__/'"$MINIKUBE_IP"'/g' ./config
+
 python start.py --url=${URL} --filter=${FILTER} \
     --iteration-number=${ITERATION_NUMBER} \
     --asset-number=${ASSET_NUMBER}
